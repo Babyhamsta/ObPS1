d96ef623a28fa197d3a7a6332c0cc70100dc64b9
Babyhamsta <Babyhamstaz@yahoo.com>
2023-08-31 15:57:22 -0500

    Create Obfuscated-Vars-Strings-FunctionNames-Junked.ps1

---
+++ Samples/Obfuscated-Vars-Strings-FunctionNames-Junked.ps1
@@ -0,0 +1,202 @@
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$ROgCHEXvqNuQUYJbilLFeIDBZGPApkKo = [math]::E
+function LBRVSozUqNuvyhgWxHsmrGFYTbjMtlKw {
+    $lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+$IOSUqzeMVsJlNFyZdoTQbrcRmwvHEgtD = [math]::Tan(1)
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$PkQbLlSpBXrECziMTsenFWYVvJRujgdD = 0
+}
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$TLVFqCypiGjUbxERdKNagmIQAYvloDXJ = 'a' -ne 'b'
+LBRVSozUqNuvyhgWxHsmrGFYTbjMtlKw
+$TaLstHbCmUBMIdpejDFlzARhfwyQSxGu = 'moreRandom'
+# Hi there this is a sample script, do note this is subject to change!
+$hIQzLbUsnkleRGZOirwFoxKBtDuSWVmN = 3 / 1
+$TaLstHbCmUBMIdpejDFlzARhfwyQSxGu = 'moreRandom'
+function akrWuVQvxnwisceTzIBpjPUGZbOENmho {
+    $pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$dVNFHvDUKwYMTjlumfLOxkIpbSezAaqX = 'txt'
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$RWUXlGwkJLzdFTIigOfHMuZAyshSPbCB = [math]::Abs(-1)
+$TLVFqCypiGjUbxERdKNagmIQAYvloDXJ = 'a' -ne 'b'
+$ORKCMrTySAcZxmspQBUPkjYNwgahEiFD = [math]::Sign(1)
+$pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+$XfLoIFeYnEkzRwaCSxtPcdGBpZbhrygj = [math]::Cos(1)
+$ORKCMrTySAcZxmspQBUPkjYNwgahEiFD = [math]::Sign(1)
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+$PkQbLlSpBXrECziMTsenFWYVvJRujgdD = 0
+$TLVFqCypiGjUbxERdKNagmIQAYvloDXJ = 'a' -ne 'b'
+$PkQbLlSpBXrECziMTsenFWYVvJRujgdD = 0
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+$hIQzLbUsnkleRGZOirwFoxKBtDuSWVmN = 3 / 1
+$UKoubPeYIvxyMCVzZgwdiBpkStRsQEGc = 1 -lt 2
+$UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+}
+$dVNFHvDUKwYMTjlumfLOxkIpbSezAaqX = 'txt'
+akrWuVQvxnwisceTzIBpjPUGZbOENmho
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmK = "$([char]([byte]0x49))$([char]([byte]0x63))$([char]([byte]0x65))$([char]([byte]0x20))$([char]([byte]0x63))$([char]([byte]0x72))$([char]([byte]0x65))$([char]([byte]0x61))$([char]([byte]0x6D))$([char]([byte]0x20))$([char]([byte]0x77))$([char]([byte]0x6F))$([char]([byte]0x75))$([char]([byte]0x6C))$([char]([byte]0x64))$([char]([byte]0x20))$([char]([byte]0x62))$([char]([byte]0x65))$([char]([byte]0x20))$([char]([byte]0x67))$([char]([byte]0x72))$([char]([byte]0x65))$([char]([byte]0x61))$([char]([byte]0x74))$([char]([byte]0x20))$([char]([byte]0x72))$([char]([byte]0x69))$([char]([byte]0x67))$([char]([byte]0x68))$([char]([byte]0x74))$([char]([byte]0x20))$([char]([byte]0x6E))$([char]([byte]0x6F))$([char]([byte]0x77))"
+$LfrxpCbMYOihQgkFtvEyasmoJZPjeSlq = -not $havDsMwJxASzNOgELFVfBtbKWmIjiQCe
+$RWUXlGwkJLzdFTIigOfHMuZAyshSPbCB = [math]::Abs(-1)
+function XgHMVbmfQPsdtYBKaSuIzoyneDqpJZvx {
+    $pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+$LfrxpCbMYOihQgkFtvEyasmoJZPjeSlq = -not $havDsMwJxASzNOgELFVfBtbKWmIjiQCe
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+$XfLoIFeYnEkzRwaCSxtPcdGBpZbhrygj = [math]::Cos(1)
+$hIQzLbUsnkleRGZOirwFoxKBtDuSWVmN = 3 / 1
+$UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+}
+$UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+XgHMVbmfQPsdtYBKaSuIzoyneDqpJZvx
+$dLDtNaQuBrKOiFGCMljEywYRAmoZgfvs = 'x' * 2
+
+function YgMJraxIvnKDVHWdbEqmLtUTSpzwNjOP()
+{
+function bTmDcZwPzKdLkvBXSOYaGpVRrlNQqIHu {
+    $pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+$ORKCMrTySAcZxmspQBUPkjYNwgahEiFD = [math]::Sign(1)
+$qOYwnNlHvxRJTBsdEFugQjhaCbpmezSo = $havDsMwJxASzNOgELFVfBtbKWmIjiQCe -or $GWYZPieutoyQdKqfgOSkcxVvAJzDBLHa
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+$ojGMfJBUybKndaAxzTLEFQecZpmsgrXW = [math]::Asin(1)
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+$ROgCHEXvqNuQUYJbilLFeIDBZGPApkKo = [math]::E
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+}
+bTmDcZwPzKdLkvBXSOYaGpVRrlNQqIHu
+$dVNFHvDUKwYMTjlumfLOxkIpbSezAaqX = 'txt'
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$qOYwnNlHvxRJTBsdEFugQjhaCbpmezSo = $havDsMwJxASzNOgELFVfBtbKWmIjiQCe -or $GWYZPieutoyQdKqfgOSkcxVvAJzDBLHa
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$UKoubPeYIvxyMCVzZgwdiBpkStRsQEGc = 1 -lt 2
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+    Write-Output "$([char]([byte]0x66))$([char]([byte]0x75))$([char]([byte]0x6E))$([char]([byte]0x63))$([char]([byte]0x74))$([char]([byte]0x69))$([char]([byte]0x6F))$([char]([byte]0x6E))$([char]([byte]0x20))$([char]([byte]0x79))$([char]([byte]0x56))$([char]([byte]0x44))$([char]([byte]0x66))$([char]([byte]0x6F))$([char]([byte]0x74))$([char]([byte]0x6B))$([char]([byte]0x6E))$([char]([byte]0x41))$([char]([byte]0x49))$([char]([byte]0x7A))$([char]([byte]0x4A))$([char]([byte]0x68))$([char]([byte]0x46))$([char]([byte]0x42))$([char]([byte]0x76))$([char]([byte]0x45))$([char]([byte]0x4B))$([char]([byte]0x4E))$([char]([byte]0x48))$([char]([byte]0x70))$([char]([byte]0x52))$([char]([byte]0x61))$([char]([byte]0x55))$([char]([byte]0x5A))$([char]([byte]0x69))$([char]([byte]0x62))$([char]([byte]0x6A))$([char]([byte]0x51))$([char]([byte]0x4C))$([char]([byte]0x53))$([char]([byte]0x63))$([char]([byte]0x20))$([char]([byte]0x68))$([char]([byte]0x69))$([char]([byte]0x74))$([char]([byte]0x21))"
+    return "$([char]([byte]0x57))$([char]([byte]0x68))$([char]([byte]0x79))$([char]([byte]0x20))$([char]([byte]0x61))$([char]([byte]0x72))$([char]([byte]0x65))$([char]([byte]0x20))$([char]([byte]0x77))$([char]([byte]0x65))$([char]([byte]0x20))$([char]([byte]0x6F))$([char]([byte]0x62))$([char]([byte]0x66))$([char]([byte]0x75))$([char]([byte]0x73))$([char]([byte]0x63))$([char]([byte]0x61))$([char]([byte]0x74))$([char]([byte]0x69))$([char]([byte]0x6E))$([char]([byte]0x67))$([char]([byte]0x20))$([char]([byte]0x61))$([char]([byte]0x20))$([char]([byte]0x73))$([char]([byte]0x63))$([char]([byte]0x72))$([char]([byte]0x69))$([char]([byte]0x70))$([char]([byte]0x74))$([char]([byte]0x20))$([char]([byte]0x61))$([char]([byte]0x6E))$([char]([byte]0x79))$([char]([byte]0x77))$([char]([byte]0x61))$([char]([byte]0x79))$([char]([byte]0x73))$([char]([byte]0x3F))$([char]([byte]0x20))$([char]([byte]0x57))$([char]([byte]0x68))$([char]([byte]0x6F))$([char]([byte]0x20))$([char]([byte]0x77))$([char]([byte]0x6F))$([char]([byte]0x75))$([char]([byte]0x6C))$([char]([byte]0x64))$([char]([byte]0x20))$([char]([byte]0x77))$([char]([byte]0x61))$([char]([byte]0x6E))$([char]([byte]0x74))$([char]([byte]0x20))$([char]([byte]0x74))$([char]([byte]0x6F))$([char]([byte]0x20))$([char]([byte]0x72))$([char]([byte]0x65))$([char]([byte]0x61))$([char]([byte]0x64))$([char]([byte]0x20))$([char]([byte]0x6D))$([char]([byte]0x79))$([char]([byte]0x20))$([char]([byte]0x63))$([char]([byte]0x6F))$([char]([byte]0x64))$([char]([byte]0x65))$([char]([byte]0x3F))"
+$LfrxpCbMYOihQgkFtvEyasmoJZPjeSlq = -not $havDsMwJxASzNOgELFVfBtbKWmIjiQCe
+$PZegARxsSfQXGmvBuCndYyWTKDhLirJU = [math]::Round(1)
+function karAhMDgGFyOpsCTQHNYwVWKmXxSblnR {
+    $lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+$dLDtNaQuBrKOiFGCMljEywYRAmoZgfvs = 'x' * 2
+$ojGMfJBUybKndaAxzTLEFQecZpmsgrXW = [math]::Asin(1)
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+$LfrxpCbMYOihQgkFtvEyasmoJZPjeSlq = -not $havDsMwJxASzNOgELFVfBtbKWmIjiQCe
+$PkQbLlSpBXrECziMTsenFWYVvJRujgdD = 0
+}
+$ROgCHEXvqNuQUYJbilLFeIDBZGPApkKo = [math]::E
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+$TaLstHbCmUBMIdpejDFlzARhfwyQSxGu = 'moreRandom'
+karAhMDgGFyOpsCTQHNYwVWKmXxSblnR
+function eOTVyYXnFpUDsZAWLwSKBbzIifCoGjcl {
+    $lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+$lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+$ORKCMrTySAcZxmspQBUPkjYNwgahEiFD = [math]::Sign(1)
+$dLDtNaQuBrKOiFGCMljEywYRAmoZgfvs = 'x' * 2
+$PZegARxsSfQXGmvBuCndYyWTKDhLirJU = [math]::Round(1)
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+}
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$PZegARxsSfQXGmvBuCndYyWTKDhLirJU = [math]::Round(1)
+$LfrxpCbMYOihQgkFtvEyasmoJZPjeSlq = -not $havDsMwJxASzNOgELFVfBtbKWmIjiQCe
+eOTVyYXnFpUDsZAWLwSKBbzIifCoGjcl
+}
+
+function mbrqOhDyAzSxYaTpjdeFZPngLBQkHNCR {
+    $LfrxpCbMYOihQgkFtvEyasmoJZPjeSlq = -not $havDsMwJxASzNOgELFVfBtbKWmIjiQCe
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$TaLstHbCmUBMIdpejDFlzARhfwyQSxGu = 'moreRandom'
+$qOYwnNlHvxRJTBsdEFugQjhaCbpmezSo = $havDsMwJxASzNOgELFVfBtbKWmIjiQCe -or $GWYZPieutoyQdKqfgOSkcxVvAJzDBLHa
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$XfLoIFeYnEkzRwaCSxtPcdGBpZbhrygj = [math]::Cos(1)
+$PkQbLlSpBXrECziMTsenFWYVvJRujgdD = 0
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+$dLDtNaQuBrKOiFGCMljEywYRAmoZgfvs = 'x' * 2
+$ojGMfJBUybKndaAxzTLEFQecZpmsgrXW = [math]::Asin(1)
+$PZegARxsSfQXGmvBuCndYyWTKDhLirJU = [math]::Round(1)
+}
+$pCoEBLlAxrjceznNYbkRtMsODSIqKZTh = 'rnd'
+mbrqOhDyAzSxYaTpjdeFZPngLBQkHNCR
+YgMJraxIvnKDVHWdbEqmLtUTSpzwNjOP
+function CJDFTlWLukawYEGbnSiyxopZOMVdRvhr {
+    $ojGMfJBUybKndaAxzTLEFQecZpmsgrXW = [math]::Asin(1)
+$rUanklMvTziCqGQgyBHoWwXOxtVjeIdN = [math]::Pow(1,2)
+$rUanklMvTziCqGQgyBHoWwXOxtVjeIdN = [math]::Pow(1,2)
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+}
+CJDFTlWLukawYEGbnSiyxopZOMVdRvhr
+$hIQzLbUsnkleRGZOirwFoxKBtDuSWVmN = 3 / 1
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+
+if ($JgXelGbDrZdNzjPQOwMYkiCosTyVSAmK -match "$([char]([byte]0x49))$([char]([byte]0x63))$([char]([byte]0x65))")
+{
+$XfLoIFeYnEkzRwaCSxtPcdGBpZbhrygj = [math]::Cos(1)
+function KrwgiczONTkXRMhptWJlGxEUdYICBDsn {
+    $UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$rUanklMvTziCqGQgyBHoWwXOxtVjeIdN = [math]::Pow(1,2)
+$IaYtbHehzsiwDXPdQGgkNBSvOUVnJxmc = 1
+$eNjXdLbEFOizpsKxRorCMuyVPmGwfqDg = 2 -eq 2
+$ORKCMrTySAcZxmspQBUPkjYNwgahEiFD = [math]::Sign(1)
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$CZAPrIdWQMgkHzcGphoUnBxfJSOFTbwR = [math]::Sqrt(1)
+$UKoubPeYIvxyMCVzZgwdiBpkStRsQEGc = 1 -lt 2
+$JgXelGbDrZdNzjPQOwMYkiCosTyVSAmKF5G6 = $BnDmsVLIKWRGtrguowHaEFCOyiXvqeMx
+$ZxqUYIukmocbAQNOJrXFjPnBGaiWEVdL = 2 + 2
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$ORKCMrTySAcZxmspQBUPkjYNwgahEiFD = [math]::Sign(1)
+$dVNFHvDUKwYMTjlumfLOxkIpbSezAaqX = 'txt'
+$UqHWvQNKmockXzRDaFgTtpiIyhrLEGeP = [math]::Log(2)
+$PZegARxsSfQXGmvBuCndYyWTKDhLirJU = [math]::Round(1)
+$TLVFqCypiGjUbxERdKNagmIQAYvloDXJ = 'a' -ne 'b'
+$dVNFHvDUKwYMTjlumfLOxkIpbSezAaqX = 'txt'
+}
+$dLDtNaQuBrKOiFGCMljEywYRAmoZgfvs = 'x' * 2
+$lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+$MZJhsctjgOSDrFNCpuRkzoAfQqXUWaKE = [math]::Pi
+KrwgiczONTkXRMhptWJlGxEUdYICBDsn
+$TLVFqCypiGjUbxERdKNagmIQAYvloDXJ = 'a' -ne 'b'
+$bvWKrLEpFQmJUxBXeVyARTiOsoSIazuN = 1..2
+    Write-Output "$([char]([byte]0x4E))$([char]([byte]0x6F))$([char]([byte]0x77))$([char]([byte]0x20))$([char]([byte]0x74))$([char]([byte]0x68))$([char]([byte]0x61))$([char]([byte]0x74))$([char]([byte]0x27))$([char]([byte]0x73))$([char]([byte]0x20))$([char]([byte]0x65))$([char]([byte]0x70))$([char]([byte]0x69))$([char]([byte]0x63))"
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+function XpjyKqYOrLHmlAGckZVeivPoIFbSWNJB {
+    $CZAPrIdWQMgkHzcGphoUnBxfJSOFTbwR = [math]::Sqrt(1)
+$PkQbLlSpBXrECziMTsenFWYVvJRujgdD = 0
+$IOSUqzeMVsJlNFyZdoTQbrcRmwvHEgtD = [math]::Tan(1)
+$IaYtbHehzsiwDXPdQGgkNBSvOUVnJxmc = 1
+$dLDtNaQuBrKOiFGCMljEywYRAmoZgfvs = 'x' * 2
+$lRDONonTQYjALqSdhGBJUyIXKHPpkFCv = 4 % 3
+$ZBMcDSpumytLJGidNkfOVRXwzUKrTIPA = 3 - 1
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$ZviSnsMUwxLcrIDdAYGXtfeBQuPgVRbk = 'randomString'
+$TLVFqCypiGjUbxERdKNagmIQAYvloDXJ = 'a' -ne 'b'
+$CZAPrIdWQMgkHzcGphoUnBxfJSOFTbwR = [math]::Sqrt(1)
+$CZAPrIdWQMgkHzcGphoUnBxfJSOFTbwR = [math]::Sqrt(1)
+}
+XpjyKqYOrLHmlAGckZVeivPoIFbSWNJB
+}
+
+<#
+Output:
+
+function yVDfotknAIzJhFBvEKNHpRaUZibjQLSc hit!
+Why are we obfuscating a script anyways? Who would want to read my code?
+Now that's epic
+
+#>
